6827f8cc0640bab3c0e113e1de3444854fc656bc
Aiikon <justin.coon@gmail.co>
2019-06-16 15:33:38 -0700

    Create Tests.ps1

---
+++ Tests.ps1
@@ -0,0 +1,45 @@
+Import-Module (Get-Module DB).Path -Force -DisableNameChecking
+
+Initialize-DBConnectionToLocalDB DBTest -FilePath C:\Temp\DBTest.mdf
+
+Remove-DBTable DBTest -Table Cluster
+New-DBTable DBTest -Table Cluster -Definition {
+    Define-DBColumn ClusterId int -Required -PrimaryKey
+    Define-DBColumn ClusterName nvarchar -Required -Length 15
+    Define-DBColumn ClusterType nvarchar
+}
+
+@"
+ClusterId,ClusterName,ClusterType
+1,SQL001,SQL
+2,SQL002,SQL
+3,CAFile,File
+4,TXFile,File
+6,SQL003,SQL
+"@ | ConvertFrom-Csv | Add-DBRow DBTest -Table Cluster
+
+Get-DBRow DBTest -Table Cluster -Verbose | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterEq @{ClusterType='SQL'} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterEq @{ClusterType='SQL','File'} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterNe @{ClusterType='SQL','File'} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterLike @{ClusterType='SQL','File'} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterNotLike @{ClusterType='SQL','File'} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterGt @{ClusterId=1} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterGe @{ClusterId=1} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterLt @{ClusterId=1} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterLe @{ClusterId=1} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterGe @{ClusterId=1} -FilterLe @{ClusterId=3} | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterNull ClusterType | Out-Default
+
+Get-DBRow DBTest -Table Cluster -Verbose -FilterNotNull ClusterType | Out-Default
